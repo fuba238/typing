297c2ccb0e5d7431cba7af50bc7a64fc8b10ff62
史哉 <65520530+fuba238@users.noreply.github.com>
2022-02-14 06:46:24 +0000

    update

--- main.js
+++ main.js
@@ -8,10 +8,38 @@ const questions = [
 
 const question = document.querySelector('.question');
 const counter = document.querySelector('.counter');
-const space = document.querySelector('.space');
+const button = document.querySelector('.button');
 
-let num = questions.length;
-document.addEventListener("keydown", keyDown);
-function keyDown(e){
+button.addEventListener('click', () => {
+  
+});
 
+function countdown(){
+  const totalTime = 3000; // 10秒
+  const oldTime = Date.now();
+
+  const timerId = setInterval(() => {
+  const currentTime = Date.now();
+  // 差分を求める
+  const diff = currentTime - oldTime;
+
+  // 残りミリ秒を計算する
+  const remainMSec = totalTime - diff;
+  // ミリ秒を整数の秒数に変換する
+  const remainSec = Math.ceil(remainMSec / 1000);
+
+  let label = `${remainSec}`;
+
+  // 0秒以下になったら
+  if (remainMSec <= 0) {
+    // タイマーを終了する
+    clearInterval(timerId);
+
+    // タイマー終了の文言を表示
+    label = 'スタート';
+  }
+
+  // 画面に表示する
+  document.querySelector('#log').innerHTML = label;
+}, 1000);
 }
